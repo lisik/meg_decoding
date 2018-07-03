39de109c32ab9950357d3f3d7c46073cc9db07fb
Leyla Isik <lisik@openmind7.mit.edu>
2018-07-03 09:54:19 -0400

    added function arguments to run null decoding/which iterations of null decoding

--- run_decoding_itx.sh
+++ run_decoding_itx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-matlab -nodesktop -nodisplay -nosplash -singleCompThread -r "run run_decoding_itx; exit;"
+matlab -nodesktop -nodisplay -nosplash -singleCompThread -r "run run_decoding_itx($1, $2); exit;"
