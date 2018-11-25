9ffb1261a462285f617c13c626064a025d5ef3ac
Leyla Isik <lisik@mit.edu>
2018-11-25 15:56:21 -0500

    add eye tracking argument to bash script

--- run_decoding_itx_gen.sh
+++ run_decoding_itx_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-matlab -nodesktop -nodisplay -nosplash -singleCompThread -r "run run_decoding_itx_gen($1); exit;"
+matlab -nodesktop -nodisplay -nosplash -singleCompThread -r "run run_decoding_itx_gen($1,$2); exit;"
